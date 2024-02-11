0bf33a27a49f4b58c7b66d54a4312cb7d90c7455
Noah Math <nm55601@gmail.com>
2024-02-10 20:25:37 -0800

    Revert "Score works correctly"
    This reverts commit 0bb4e4a8def85bc11b6d8059933927757d23b5c6.

--- site/script.js
+++ site/script.js
@@ -22,7 +22,6 @@ var audioFiles = ['http://localhost:8000/trimmed/0Winter-Wind - Evgeny Kissin.mp
 
 sitePrefix = 'http://localhost:8000/trimmed/';
 
-rounds = 5;
 window.roundNumber = 0;
 window.score = 0;
 let clonedFiles = JSON.parse(JSON.stringify(audioFiles));
@@ -49,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log("Wrong");
                 alert("Wrong!");
             }
-            window.roundNumber += 1;
+            roundNumber += 1;
             loadButtons(getCorrectButton());
   });
 });
@@ -63,12 +62,8 @@ function getCorrectButton() {
     return correct_button
 }
 function loadButtons(correct_button) {
-    if (roundNumber == rounds) {
-        localStorage.setItem('score', window.score);
-        localStorage.setItem('round', window.roundNumber);
-        console.log(window.roundNumber);
+    if (roundNumber == 5) {
         loadSeparateHTMLFile();
-        return;
     }
     var buttons = Array.from(document.querySelectorAll('.button'));
     //set audio source to random file
@@ -107,5 +102,13 @@ function formatButtonContent(audioSrc) {
 }
 
 function loadSeparateHTMLFile() {
-    window.location.href = 'EndGame.html';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'EndGame.html', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        // Inject the HTML content into the current document
+        document.body.innerHTML += xhr.responseText;
+      }
+    };
+    xhr.send();
   }
